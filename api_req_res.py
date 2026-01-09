4eaa539447c8bd51b3435b13eadf1fef059c775e
Rohit Paradkar <rohit.paradkar@transformhub.com>
2026-01-09 12:22:05 +0530

    metadat plus channel data integration

--- api_req_res.py
+++ api_req_res.py
@@ -1,1 +1,45 @@
-from api_call import upload_response
+from api_call import (
+    upload_document_to_dolphin_dms,
+    download_document_from_dolphin_dms,
+)
+
+policy_data = {
+  "channel": "Worksite",
+  "policy_number": "35091132202500",
+  "insured_name": "Rohit",
+  "insured_contact_no": "33",
+  "insurance_company_name": "Niva Bupa",
+  "products": "4w",
+  "policy_start_date": "02/02/2026",
+  "policy_expiry_date": "02/02/2027",
+  "sum_assured_or_idv": "44",
+  "net_premium": "55",
+  "vehicle_registration_no": "66",
+  "business_or_retention_type": "Renewal"
+}
+
+
+# response = upload_document_to_dolphin_dms(
+#     file_path="./data/test_data/data/NivaBupa/35091132202500.pdf",
+#     policy_data=policy_data,
+# )
+
+# # PRINT FULL SERVER RESPONSE
+# print("Upload Response from Server:")
+# print(response)
+
+# #  OPTIONAL: PRINT ONLY MONGO ID
+# if isinstance(response, dict) and "mongoId" in response:
+#     print("mongoId", response["mongoId"])
+
+
+#=======================================download===============================
+mongo_id = "695fb152940eb40503ae57a1"
+response = download_document_from_dolphin_dms(
+        mongo_id=mongo_id,
+        save_path="downloads/policy.pdf",
+    )
+
+# PRINT FULL SERVER RESPONSE
+print("Download Response from Server:")
+print(response)
--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 from img_xml_text_extractor import extract_text_from_pdf_via_svg_all_pages
 from gemini_extractor import extract_insurance_metadata
 import json
+from api_call import (
+    upload_document_to_dolphin_dms,
+    download_document_from_dolphin_dms,
+)
+def filter_metadata(metadata: dict) -> dict:
+    """
+    Pick only required fields from extracted metadata
+    """
+    required_fields = [
+  "Policy_Number",
+  "Insured_Name",
+  "Insured_Contact_No",
+  "Insurance_Company_Name",
+  "Previous_Insurance_Company",
+  "Products",
+  "Policy_Start_Date",
+  "Policy_Expiry_Date",
+  "Sum_Assured_OR_IDV",
+  "Net_Premium",
+  "Vehicle_Registration_No",
+  "Business_Or_Retention_Type",
+    ]
 
+    return {
+        key: metadata.get(key)
+        for key in required_fields
+        if key in metadata and metadata.get(key) not in ("", None)
+    }
+
+def get_additional_fields() -> dict:
+    return {
+        "Channel": "Worksite",
+    }
+
+def build_policy_payload(metadata: dict) -> dict:
+    filtered_metadata = filter_metadata(metadata)
+    additional_fields = get_additional_fields()
+
+    combined_payload = {
+        **filtered_metadata,
+        **additional_fields
+    }
+
+    return combined_payload
 def main():
     """
     Provide the file path from local system in below variable
@@ -16,7 +59,23 @@ def main():
     print("Total number of characters in document:", len(text))
 
     metadata = extract_insurance_metadata(text)
+    policy_data = build_policy_payload(metadata)
+    print("Final Schema:",policy_data)
+
     print(json.dumps(metadata, ensure_ascii=False, indent=2))
+
+    response = upload_document_to_dolphin_dms(
+    file_path=PDF_PATH,
+    policy_data=policy_data
+    )
+    # PRINT FULL SERVER RESPONSE
+    print("Upload Response from Server:")
+    print(response)
+
+    #  OPTIONAL: PRINT ONLY MONGO ID
+    if isinstance(response, dict) and "mongoId" in response:
+        print("mongoId", response["mongoId"])
+
 
 
 
